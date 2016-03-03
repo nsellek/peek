b237a60f41247d199841f35e517efc8458cd4fa2
Aldrin Bustos <multpod@yahoo.com>
2016-03-03 09:57:48 -0500

    more styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-<<<<<<< HEAD
-  validates :zipcode, presence: true
-=======
->>>>>>> 7eef255614a339e4d73edd8d9c60f426e9da9e70
 	validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
 
@@ -12,6 +8,5 @@ class User < ActiveRecord::Base
 	has_many :house_users
 	has_many :houses, through: :house_users
 	has_secure_password
-
 	has_many :pictures
 end
